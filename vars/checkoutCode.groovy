8e39bf0bbd001b4c4b21f8dae2b52f8f2ee38678
sidhdhakal <siddharthadhakal54@gmail.com>
2026-05-10 12:02:43 +0545

    complete CI shared library

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,4 +1,11 @@
 def call(String repoUrl, String branch = 'main') {
-    echo "Checking out code from ${repoUrl} branch: ${branch}"
-    git branch: branch, url: repoUrl
+    echo "📥 Checking out code from ${repoUrl} branch: ${branch}"
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        userRemoteConfigs: [[
+            url: repoUrl,
+            credentialsId: 'github-credentials'  // ← match the ID
+        ]]
+    ])
 }
