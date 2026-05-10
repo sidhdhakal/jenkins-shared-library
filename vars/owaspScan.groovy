063aa7f010c8363329a76c394de269e28636adf2
sidhdhakal <siddharthadhakal54@gmail.com>
2026-05-10 12:38:47 +0545

    complete CI shared library

--- vars/owaspScan.groovy
+++ vars/owaspScan.groovy
@@ -1,25 +1,17 @@
 def call() {
-    echo " Running OWASP Dependency Check..."
+    echo "🔍 Running OWASP Dependency Check..."
     dependencyCheck(
         additionalArguments: '''
             --scan ./
             --format HTML
             --format XML
             --out ./owasp-report
-            --prettyPrint
         ''',
         odcInstallation: 'OWASP-DC'
     )
     dependencyCheckPublisher(
         pattern: 'owasp-report/dependency-check-report.xml',
-        stopBuild: false,
-        failedTotalCritical: 1,
-        warnedTotalHigh: 5
+        stopBuild: false,          // ← don't stop pipeline
+        failedTotalCritical: 1
     )
-    publishHTML([
-        allowMissing: false,
-        reportDir: 'owasp-report',
-        reportFiles: 'dependency-check-report.html',
-        reportName: 'OWASP Dependency Report'
-    ])
 }
